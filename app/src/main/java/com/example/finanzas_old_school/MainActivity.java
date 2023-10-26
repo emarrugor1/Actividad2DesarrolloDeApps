26d1ecefba4d31cb8a5426f2bf2230aecf5cfdba
Edwin Marrugo <edwinmarr@hotmail.com>
2023-10-25 21:59:29 -0500

    Se implementa Toolbar

--- app/src/main/java/com/example/finanzas_old_school/MainActivity.java
+++ app/src/main/java/com/example/finanzas_old_school/MainActivity.java
@@ -2,21 +2,45 @@ package com.example.finanzas_old_school;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbarCategory);
+        setSupportActionBar(toolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.categoryItem) {
+            Intent intent = new Intent(this, this.getClass());
+            startActivity(intent);
+        } else if (id == R.id.movementsItem) {
+            Intent intent = new Intent(this, IncomesAndExpensesForm.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     public void goToMovementsForm(View view){
         Intent intent = new Intent(this, IncomesAndExpensesForm.class);
